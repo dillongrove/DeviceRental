19ffa3fccf557c5312cc464c8694fcb67f97afbc
webmaster <webmaster@webapps.club.cc.cmu.edu>
2012-05-27 00:02:56 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentalApp::Application.config.secret_token = '31a34012bafa98818c9037055c0050dd1b1ff965428019f208f455867bead24c6a31dc3c0ab86418d383651c25b9afbf857279bab45bf8758d184598f93876cc'
