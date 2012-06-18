e05e6aec7ae5345d0a4fc91780e15a4fda2e64f3
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-06-17 22:21:29 -0700

    added unit tests for brand, feature

--- test/unit/brand_test.rb
+++ test/unit/brand_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class BrandTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  should have_many :models
+  should have_many(:devices).through(:models)
+  
+  should validate_presence_of :name
+  
+  should allow_value("samsung").for(:name)
+  should allow_value("htc").for(:name)
+  should_not allow_value("").for(:name)
+  should_not allow_value(nil).for(:name)
+  
 end
--- test/unit/feature_test.rb
+++ test/unit/feature_test.rb
@@ -2,7 +2,12 @@ require 'test_helper'
 
 class FeatureTest < ActiveSupport::TestCase
   
-  should validate_presence_of :name
   should have_many :model_features
+  
+  should validate_presence_of :name
+  should allow_value("camera").for(:name)
+  should allow_value("x").for(:name)
+  should_not allow_value("").for(:name)
+  should_not allow_value(nil).for(:name)
   
 end
