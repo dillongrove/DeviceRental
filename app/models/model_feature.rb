adc0cba1213ca868ebff6ce2577dd59a44ee0c6a
Jiunn Haur Lim <codex.is.poetry@gmail.com>
2012-06-17 21:27:15 -0700

    updated model for model-feature

--- app/models/model_feature.rb
+++ app/models/model_feature.rb
@@ -3,7 +3,29 @@ class ModelFeature < ActiveRecord::Base
   attr_accessible :feature_id, :id, :model_id
   
   # Relationships --------------------------------------------------------------
+  belongs_to :feature
+  belongs_to :model
+  
   # Scopes ---------------------------------------------------------------------
+  scope :for_model { |model_id| where('model_id = ?', model_id) }
+  scope :for_feature { |feature_id| where('feature_id = ?', feature_id) }
+  
   # Validations ----------------------------------------------------------------
+  validates_presence_of :feature_id
+  validates_presence_of :model_id
+  validate :feature_must_exist
+  validate :model_must_exist
+  
+  private
+  
+  def feature_must_exist
+    return if self.feature_id.nil?
+    errors.add(:feature_id, "must be an existing feature") if Feature.find(self.feature_id).nil?
+  end
+  
+  def model_must_exist
+    return if self.model_id.nil?
+    errors.add(:model_id, "must be an existing model") if Model.find(self.model_id).nil?
+  end
   
 end
