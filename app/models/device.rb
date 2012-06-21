4322945fba38b727bd16f635a036d874345effbf
Dillon Grove <dgrove@andrew.cmu.edu>
2012-06-21 01:22:20 -0400

    Added regex validation for IMEI and MEID, added callbacks to reformat these before save, and unit tests for IMEI and MEID as well

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,6 +1,8 @@
 # Device
 class Device < ActiveRecord::Base
-
+  before_save :reformat_IMEI
+  before_save :reformat_MEID
+  
   attr_accessible :IMEI, :MEID, :active, :condition, :id, :model_id, :os_version_id
   
   # Relationships --------------------------------------------------------------
@@ -21,7 +23,8 @@ class Device < ActiveRecord::Base
   validates_uniqueness_of :IMEI, :allow_blank => true
   validates_uniqueness_of :MEID, :allow_blank => true
   
-  # TODO: format for IMEI, MEID
+  validates_format_of :IMEI, :with => /^\d{2}[-]?\d{6}[-]?\d{6}([-]?\d{1,2})?$/, :message => "should be in the format AA-BBBBBB-CCCCCC with an optional -D or -DD at the end, and contain only digits.", :allow_blank => true
+  validates_format_of :MEID, :with => /^[0-9A-Fa-f]{2}[-]?[0-9A-Fa-f]{6}[-]?[0-9A-Fa-f]{6}$/, :message => "should be in the format AA-BBBBBB-CCCCCC and contain only hexadecimal characters.", :allow_blank => true
   
   private
   
@@ -38,6 +41,20 @@ class Device < ActiveRecord::Base
   def presence_of_IMEI_or_MEID
     return if not (self.IMEI.nil? and self.MEID.nil?)
     errors.add(:IMEI, "Either IMEI or MEID must be present.")
+  end
+  
+  def reformat_IMEI
+    return if self.IMEI.nil?
+    imei = self.IMEI.to_s  # change to string in case input as all numbers 
+    imei.gsub!(/[^0-9]/,"") # strip all non-digits
+    self.IMEI = imei # reset self.IMEI to new string
+  end
+
+  def reformat_MEID
+    return if self.MEID.nil?
+    meid = self.MEID.to_s  # change to string in case input as all numbers 
+    meid.gsub!(/[^0-9A-Fa-f]/,"") # strip all non-hexadecimal characters
+    self.MEID = meid # reset self.MEID to new string
   end
   
 end
--- test/unit/device_test.rb
+++ test/unit/device_test.rb
@@ -1,14 +1,37 @@
 require 'test_helper'
 
 class DeviceTest < ActiveSupport::TestCase
-  
+  # Test relationships
   should have_many :rentals
   should belong_to :os_version
   should belong_to :model
   
+  # Test basic validations
   should validate_presence_of :model_id
   should validate_presence_of :os_version_id
   
+  # Testing MEID
+  should allow_value("A0-123456-123456").for(:MEID)
+  should allow_value("ab-abcdef-abcdef").for(:MEID)
+  should allow_value("ababcdefabcdef").for(:MEID)
+  should allow_value("12123456123456").for(:MEID)
+  should_not allow_value("121234561234567").for(:MEID) # one too many characters
+  should_not allow_value("1212345612345").for(:MEID) # one two few characters
+  should_not allow_value("ababcdefabcdeg").for(:MEID)# contains a non-hex character
+  
+  # Testing IMEI
+  should allow_value("12-123456-123456-12").for(:IMEI)
+  should allow_value("12-123456-123456-1").for(:IMEI)
+  should allow_value("12-123456-123456").for(:IMEI)
+  should allow_value("1212345612345612").for(:IMEI)
+  should allow_value("121234561234561").for(:IMEI)
+  should allow_value("12123456123456").for(:IMEI)
+  should_not allow_value("12123456123456123").for(:IMEI) # one too many characters
+  should_not allow_value("1212345612345").for(:IMEI) # one too few characters
+  should_not allow_value("A0-123456-123456").for(:IMEI) # contains a non-digit
+  should_not allow_value("12-123456-123456-").for(:IMEI) # contains a trailing dash
+  
+
   context "Creating 1 model and 1 OS version" do
   
     setup do
@@ -50,6 +73,14 @@ class DeviceTest < ActiveSupport::TestCase
       d.MEID = "49-015420-323751"
       assert d.valid?
     
+    end
+    
+    should "show that we can correctly reformat IMEIs and MEIDs" do
+      @a = FactoryGirl.create(:device, :IMEI => "49-015420-323751", :MEID => nil)
+      assert_equal "49015420323751", @a.IMEI
+      
+      @b = FactoryGirl.create(:device, :IMEI => nil, :MEID => "A0-123456-123456")
+      assert_equal "A0123456123456", @b.MEID
     end
     
     teardown do
