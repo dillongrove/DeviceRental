f643348ba875740a0878deb7aac693dd3f806e1b
Dillon Grove <dillongrove@gmail.com>
2012-12-17 04:26:49 -0500

    Added basic CRUD operatons to rentals controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,46 @@
 class RentalsController < ApplicationController
+
+  def index
+    @rentals = Rental.all
+  end
+
+  def show
+    @rental = Rental.find(params[:id])
+  end
+
+  def new
+    @rental = Rental.new
+  end
+
+  def edit
+    @rental = Rental.find(params[:id])
+  end
+
+  def create
+    @rental = Rental.new(params[:rental])
+    if @rental.save
+      flash[:notice] = "Successfully created rental."
+      redirect_to @rental
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @rental = Rental.find(params[:id])
+    if @rental.update_attributes(params[:rental])
+      flash[:notice] = "Successfully updated rental."
+      redirect_to @rental
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @rental = Rental.find(params[:id])
+    @rental.destroy
+    flash[:notice] = "Successfully destroyed rental."
+    redirect_to rentals_url
+  end
+
 end
