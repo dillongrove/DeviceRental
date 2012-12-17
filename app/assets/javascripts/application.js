a57f104247d48678283a9f9667541f13937d5cba
Dillon Grove <dillongrove@android-b34b75ce47c54f6f.wv.cc.cmu.edu>
2012-12-17 15:40:11 -0500

    Fixed an issue which caused bootstrap's js not to be loaded correctly

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+// Flash fade
+  $(function() {
+     $('.alert-success').fadeIn('normal', function() {
+        $(this).delay(3700).fadeOut();
+     });
+  });
+
+  $(function() {
+     $('.alert-error').fadeIn('normal', function() {
+        $(this).delay(3700).fadeOut();
+     });
+  });
